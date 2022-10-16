948210cbc9e8eaee6c5ef1e508033a16d41f510c
Alan Escobedo <ingescobedoalan4@gmail.com>
2022-10-16 05:37:27 -0500

    Lista de productos adaptable movil/escritorio

--- src/shop/components/ProductList.jsx
+++ src/shop/components/ProductList.jsx
@@ -3,7 +3,7 @@ import { ProductCard } from './ProductCard'
 
 export const ProductList = () => {
   return (
-    <div className='w-full grid grid-cols-2 gap-1 px-2 pb-10'>
+    <div className='w-full grid grid-cols-2 md:grid-cols-3 gap-1 px-2 pb-10'>
       {
         popular.map((product, index) => (
           <ProductCard key={index} {...product} />
--- src/shop/pages/ProductCategory.jsx
+++ src/shop/pages/ProductCategory.jsx
@@ -19,11 +19,11 @@ export const ProductCategory = () => {
         <h1>8 Productos</h1>
       </div>
       <div className='w-full h-[220px]'>
-        <img src={eibraImg} alt='Eibra Goods Photo' className='object-cover object-top w-full h-full' />
+        <img src={eibraImg} alt='Eibra Goods Photo' className='object-cover object-top md:object-center w-full h-full' />
       </div>
       <div className='py-6 px-2 flex flex-col items-center gap-2 text-center'>
         <h1 className='text-2xl'>Playeras</h1>
-        <p className='line-clamp-2 text-sm'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla inventore harum illo neque.</p>
+        <p className='line-clamp-1 text-sm'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla inventore harum illo neque.</p>
       </div>
       <ProductList />
     </div>
