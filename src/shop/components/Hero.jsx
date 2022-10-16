632b1786c8762d0243a7572f5d1abd70703dc26a
Alan Escobedo <ingescobedoalan4@gmail.com>
2022-10-16 00:27:07 -0500

    Video en el hero componente

--- src/shop/components/Hero.jsx
+++ src/shop/components/Hero.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
 
 export const Hero = () => {
+  const videoUrl = new URL('../../../assets/videos/vhs.mp4', import.meta.url).href
+
   return (
-    <div className='w-full h-screen bg-black'>
-      <h1>Hero</h1>
+    <div className='w-full h-screen bg-black relative'>
+      <video src={videoUrl} className='w-full h-full object-cover object-center' autoPlay muted loop />
+      <div className='w-auto absolute bottom-10 left-[50%] -translate-x-[50%] text-white font-gothic'>
+        <h1 className='text-5xl'>Scroll Down</h1>
+      </div>
     </div>
   )
 }
--- src/shop/ui/NavBar.jsx
+++ src/shop/ui/NavBar.jsx
@@ -1,7 +1,7 @@
 
 export const NavBar = () => {
   return (
-    <div className='w-full h-12 bg-white flex justify-between items-center'>
+    <div className='w-full h-12 bg-white flex justify-between items-center px-4'>
       <h1 className='text-2xl font-gothic'>Eibra Goods</h1>
     </div>
   )
